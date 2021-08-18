72020e97f49fd5751ef04faf00bc53ff464cfa04
JacksonT-Santos <89050572+JacksonT-Santos@users.noreply.github.com>
2021-08-18 20:07:46 -0300

    Add files via upload

---
+++ aula2.js
@@ -0,0 +1,20 @@
+//ex1console.log('O primeiro programa a gente nunca esquece!');
+
+//ex2const nome = prompt ('Digite seu nome completo:');
+//const endereco = prompt ('Digite seu endereço:');
+//const cepnumero = prompt ('Digite seu Cep e Telefone:');
+//console.log (nome+'\n'+endereco+'\n'+cepnumero);
+//console.log('Jackson Tavares Santos\nRua da Encil - 224\nCEP 42700-000 cel:(71)985224641');
+
+//ex3console.log('Seu Jeitinho Bem Faceiro \nDe tão Normal Que é \nParece uma MENINA\nMais pode ser minha mulher\nLeribi , Leribi , Leeribi\nVocê é Dona Do meu corpo\nVocê é dona de mim');
+
+//console.log('ALUNO(A)')
+
+//console.log('Cadastro de clientes');
+//console.log('0-Fim');
+//console.log('1-Inclui');
+///console.log('2-Altera');
+//console.log('3-Exclui');
+//console.log('4-Consulta');
+//const escolha = +prompt ('Digite sua opção');
+//console.log(escolha);
